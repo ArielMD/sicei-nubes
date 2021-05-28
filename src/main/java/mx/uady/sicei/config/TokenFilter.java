ad940ea3f05fe1a65ff1e4d5ed3bafc64976935d
GreenNugget <naomi.snchz99@gmail.com>
2021-05-27 22:29:12 -0500

    :coffin: useless code removed

--- src/main/java/mx/uady/sicei/config/TokenFilter.java
+++ src/main/java/mx/uady/sicei/config/TokenFilter.java
@@ -8,8 +8,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,8 +21,6 @@ import mx.uady.sicei.repository.UsuarioRepository;
 
 @Component
 public class TokenFilter extends GenericFilterBean {
-
-    private Logger log = LoggerFactory.getLogger(TokenFilter.class);
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -44,11 +40,7 @@ public class TokenFilter extends GenericFilterBean {
             return;
         }
 
-        log.info("Header de auth: [{}]", authHeader);
-
         Usuario usuario = usuarioRepository.findByToken(authHeader);
-
-        log.info("Usuario: [{}]", usuario);
 
         if (usuario == null) {
             chain.doFilter(request, response);
